ca2a5a2e586b272ad223cc2f8de8c11561b4bfcd
Kirill Sobolev <sobolevkir@bk.ru>
2025-02-06 19:46:48 +0300

    Switching from using DotLottie to Airbnb-Lottie. Loader fix.

--- app/src/main/java/com/sobolevkir/aipostcard/presentation/screen/generate/component/Loader.kt
+++ app/src/main/java/com/sobolevkir/aipostcard/presentation/screen/generate/component/Loader.kt
@@ -1,7 +1,7 @@
 package com.sobolevkir.aipostcard.presentation.screen.generate.component
 
-import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,68 +12,53 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.dotlottie.dlplayer.Mode
-import com.lottiefiles.dotlottie.core.compose.runtime.DotLottieController
-import com.lottiefiles.dotlottie.core.compose.ui.DotLottieAnimation
-import com.lottiefiles.dotlottie.core.util.DotLottieSource
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.sobolevkir.aipostcard.R
-import kotlinx.coroutines.isActive
 
 @Composable
-fun Loader(isLoading: Boolean, modifier: Modifier) {
+fun Loader(modifier: Modifier) {
 
-    val animationController = remember { DotLottieController() }
-    val alpha = remember { Animatable(0f) }
-
-    LaunchedEffect(isLoading) {
-        if (isLoading) {
-            animationController.play()
-            while (isActive) {
-                alpha.animateTo(
-                    targetValue = 1f,
-                    animationSpec = tween(durationMillis = 500, easing = LinearEasing)
-                )
-                alpha.animateTo(
-                    targetValue = 0.4f,
-                    animationSpec = tween(durationMillis = 500, easing = LinearEasing)
-                )
-            }
-        } else {
-            animationController.pause()
-            alpha.snapTo(0f)
-        }
-    }
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.Asset("animation_loading.lottie")
+    )
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        isPlaying = true,
+        iterations = LottieConstants.IterateForever
+    )
+    val alpha by animateFloatAsState(
+        targetValue = if (progress < 1f) 1f else 0.4f,
+        animationSpec = tween(durationMillis = 500, easing = LinearEasing), label = ""
+    )
 
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        DotLottieAnimation(
-            source = DotLottieSource.Asset("animation_loading.lottie"),
-            autoplay = isLoading,
-            loop = true,
-            playMode = Mode.FORWARD,
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
             modifier = Modifier
-                .alpha(if (isLoading) 1f else 0f)
                 .fillMaxWidth()
                 .wrapContentHeight(Alignment.CenterVertically)
                 .heightIn(max = 400.dp)
                 .weight(1f),
-            controller = animationController
         )
 
         Text(
             text = stringResource(R.string.message_generating),
-            color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = alpha.value),
+            color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = alpha),
             textAlign = TextAlign.Center,
             modifier = Modifier
                 .fillMaxWidth()
